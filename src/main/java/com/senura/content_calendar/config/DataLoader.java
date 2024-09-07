49f44e37a6f29504415194665a2d489ac2853aca
redshark25 <tnsenurafernando@gmail.com>
2024-09-07 23:22:29 +0200

    usage of profiles in springboot

--- src/main/java/com/senura/content_calendar/config/DataLoader.java
+++ src/main/java/com/senura/content_calendar/config/DataLoader.java
@@ -1,15 +1,17 @@
 package com.senura.content_calendar.config;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 
-//@Controller
+@Profile("!dev")
+@Controller
 public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
     //something here will be executed after dependency injection
         //can load data from here
-
+        System.out.println("Hello Data Loader");
 
     }
 }
